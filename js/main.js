d73db9442414b36971fa8746dc702f9b4e78856b
Chris Gitmed <cgitmed@gmail.com>
2020-11-10 21:00:32 +0000

    Add data model. Add event listener on the form to save data to local storage.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 var $bigNewButton = document.querySelector('#big-new-button');
-var $viewHomeEmpty = document.querySelector('.home-empty-view');
-var $viewNewEntry = document.querySelector('.new-entry-view');
+var $viewHomeEmpty = document.querySelector('#home-empty-view');
+var $viewNewEntry = document.querySelector('#new-entry-view');
+var $form = document.querySelector('form');
 
 $bigNewButton.addEventListener('click', function (event) {
   $viewHomeEmpty.className = 'home-empty-view hidden';
   $viewNewEntry.className = 'new-entry-view';
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const newBeer = {};
+  newBeer.name = $form.elements.name.value;
+  newBeer.brewery = $form.elements.brewery.value;
+  newBeer.notes = $form.elements.notes.value;
+  data.beers.push(newBeer);
+  $form.reset();
+});
+
+window.addEventListener('beforeunload', function (event) {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('beer-cellar', dataJSON);
 });
